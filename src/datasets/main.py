7c8c342ed7da4330551bbc49460a88e554f634f3
Rui Campos <mail@ruicampos.org>
2023-10-14 08:30:50 +0000

    refactor: grooming entrypoint

--- src/datasets/main.py
+++ src/datasets/main.py
@@ -6,7 +6,7 @@ import sqlite3
 from contextlib import closing
 import logging
 
-from .types import DecoratedClass, PathStr, Decorator
+from .types import DecoratedClass, Decorator
 from .pre_commit import pre_commit
 from .pull import pull
 from .push import push
@@ -17,16 +17,14 @@ logging.basicConfig(level=logging.DEBUG)
 
 logger = logging.getLogger("datasets")
 
-
-
-def dataset(
-    sql_file: PathStr = "./dataset.sqlite",
-    data_dir: PathStr = "./dataset",
-) -> Decorator:
+def dataset(*, remote: str) -> Decorator:
     """
     A decorator to manage the lifecycle of the SQLite connection 
     and initialize a dataset with the provided schema.
     """
+
+    sql_file = "./dataset.sql"
+    data_dir = "./dataset"
 
     def decorator(cls: DecoratedClass) -> DecoratedClass:
         with closing(sqlite3.connect(sql_file)) as conn:
@@ -36,14 +34,12 @@ def dataset(
                 arg = "commit"
 
                 if arg == "commit":
-                    pre_commit(cls, cursor, data_dir)
+                    pre_commit(cls, cursor, data_dir, remote)
                 elif arg == "pull":
-                    pull(cls, cursor, data_dir)
+                    pull(cls, cursor, data_dir, remote)
                 elif arg == "push":
-                    push(cls, cursor, data_dir)
+                    push(cls, cursor, data_dir, remote)
                 elif arg == "checkout":
-                    checkout(cls, cursor, data_dir)
-
+                    checkout(cls, cursor, data_dir, remote)
             return cls
-
     return decorator
