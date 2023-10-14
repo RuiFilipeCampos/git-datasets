60dab11740e9b2b625884e1f5bc6672dd3b386d9
Rui Campos <mail@ruicampos.org>
2023-10-14 08:25:53 +0000

    refactor: moving stuff around

---
+++ src/datasets/constants.py
@@ -0,0 +1,11 @@
+
+from .types import SQLTypeStrLit
+
+PYTHON_TO_SQLITE3: dict[type, SQLTypeStrLit] = {
+    str: "TEXT",
+    int: "INTEGER",
+    float: "REAL",
+    bytes: "BLOB",
+    bool: "INTEGER",
+    None: "NULL"
+}
--- src/datasets/types.py
+++ src/datasets/types.py
@@ -16,3 +16,22 @@ DiffSchema = dict[Literal["add", "remove"], list[FieldNameStr]]
 SQLTypeStrLit = Literal["TEXT", "INTEGER", "REAL", "BLOB", "NULL"]
 
 DatasetSchema = dict[FieldNameStr, SQLTypeStrLit]
+
+
+def validate_arguments(func: Callable) -> Callable:
+    """ Runtime validation of function arguments. """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        hints = get_type_hints(func)
+        for name, value in kwargs.items():
+            expected_type = hints.get(name)
+
+            if expected_type is None:
+                continue
+
+            if not isinstance(value, expected_type):
+                raise InvalidInputType(name, expected_type, value)
+
+        return func(*args, **kwargs)
+    return wrapper
