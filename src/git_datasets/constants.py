b74016bf1a9594691317f250e3ab20f720591d89
Rui Campos <mail@ruicampos.org>
2023-10-22 07:44:06 +0000

    refactor: remove sqlite

--- src/git_datasets/constants.py
+++ src/git_datasets/constants.py
@@ -1,12 +1,2 @@
 """ constants.py - Central repository for constants used throughout the application """
 
-from git_datasets.types import SQLTypeStrLit
-
-PYTHON_TO_SQLITE3: dict[type, SQLTypeStrLit] = {
-    str: "TEXT",
-    int: "INTEGER",
-    float: "REAL",
-    bytes: "BLOB",
-    bool: "INTEGER",
-    None: "NULL"
-}
