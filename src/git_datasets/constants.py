530c9627f8b1a256482afd5150ef435a125acb4d
Rui Campos <mail@ruicampos.org>
2023-10-22 10:25:57 +0000

    refactor: remove mentions of pyarrow from module level scope

--- src/git_datasets/constants.py
+++
@@ -1,9 +0,0 @@
-""" constants.py - Central repository for constants used throughout the application """
-
-import pyarrow as pa
-
-PYTHON_TO_PARQUET = {
-    str: pa.string(),
-    int: pa.int32(),
-}
-
--- src/git_datasets/parquet.py
+++ src/git_datasets/parquet.py
@@ -5,14 +5,24 @@ from git_datasets.logging import get_logger
 
 logger = get_logger(__name__)
 
+
 class ParquetFileHandler:
     """ TODO """
 
     def __init__(self, parquet_file: str):
         """ TODO """
+
+        import pyarrow as pa
+        self.python_to_arrow = {
+            str: pa.string(),
+            int: pa.int32(),
+        }
+
         if not os.path.exists(parquet_file):
             logger.debug("Parquet file does not exist")
 
     def get_schema(self):
         """ TODO """
         return {}
+
+
